8c77c210eb34bf5437e3baca28750ec859341185
jditto2 <joshkditto@gmail.com>
2025-02-21 23:35:29 -0700

    This is for the Mission 7

--- obj/Debug/net8.0/Mission6Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission6Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac54b9d0853b8df12bbd221e58dec5df9021adca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08119cf4ca649899838cda9a831e30b713cb1569")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
